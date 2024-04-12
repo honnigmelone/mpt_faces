31263784b00de9085b244ec9cee545a1155c624b
honnigmelone <nikita.rosik@study.hs-duesseldorf.de>
2024-04-12 18:03:05 +0200

    Fixed counter and commented google drive download out for now

--- record.py
+++ record.py
@@ -17,26 +17,28 @@ from common import ROOT_FOLDER
 
 # This is the data recording pipeline
 def record(args):
-    # TODO: Implement the recording stage of your pipeline
-    #   Create missing folders before you store data in them (os.mkdir)
-    
+    # TODO: Implement the recording stage of your pipeline  
     # Create missing folders before you store data in them
     target_folder = os.path.join(ROOT_FOLDER, args)
     os.makedirs(target_folder, exist_ok=True)
 
-    HAAR_CASCADE = cv.data.haarcascades + "haarcascade_frontalface_default.xmlx"
-
-    #google drive download
-    if not os.path.isfile(HAAR_CASCADE):
-        print("File not found. Downloading from google drive...")
-        url = "https://drive.google.com/file/d/1ewEfKeYhdmrBMXPItGBJ4e6h5sjkP_HR/view?usp=drive_link"
-        gdown.download(url, HAAR_CASCADE
-                       )        
-    #   Open The OpenCV VideoCapture Device to retrieve live images from your webcam (cv.VideoCapture)
+    HAAR_CASCADE = cv.data.haarcascades + "haarcascade_frontalface_default.xml"
+    
+    # #google drive download 
+    # if not os.path.isfile(HAAR_CASCADE):
+    #     print("File not found. Downloading from google drive...")
+    #     url = "https://drive.google.com/file/d/1jNQwJ6QmFCLVyUsFFIj3xkJES5T3EMz8/view"
+    #     output = os.path.join(os.getcwd(), "haarcascade_frontalface_default.xml")
+    #     gdown.download(url, output, fuzzy=True)
+    #     HAAR_CASCADE = output
+        
+        
+    # open webcam
     cap = cv.VideoCapture(0)
     if not cap.isOpened():
         print("Cannot open camera!")
         exit()
+
     #variable to detect when to cpature frame 
     frames_since_detection = 0 
     save_frames = True
@@ -60,43 +62,40 @@ def record(args):
         faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
 
         #if detected face
-        if len(faces) > 0:
-            
+        if len(faces) > 0:    
             for (x,y,w,h) in faces:
 
                 #show rectangle of face
                 cv.rectangle(frame_with_rectangle, (x,y), (x+w,y+h), (0,255,0), 2)
                 cv.putText(frame_with_rectangle,args,(x,y-10), cv.FONT_HERSHEY_COMPLEX, 0.9 ,(0,255,0), 2)
 
-                if save_frames:
+        if len(faces) and save_frames:
 
-                    #save frame with unique filename
-                    filename = f"face_{args}_{uuid.uuid4()}"
-                    
-                    # save frame with the same filename but with .jpg extension
-                    cv.imwrite(os.path.join(target_folder, f"{filename}.jpg"), frame)
+            #save frame with unique filename
+            filename = f"face_{args}_{uuid.uuid4()}"
+            
+            # save frame with the same filename but with .jpg extension
+            cv.imwrite(os.path.join(target_folder, f"{filename}.jpg"), frame)
 
-                    # write face position to a CSV file with the same filename but with .csv extension
-                    with open(os.path.join(target_folder, f"{filename}.csv"), "w", newline="") as csv_file:
-                        csv_writer = csv.writer(csv_file, delimiter=",")
-                        for x,y,w,h in faces:
-                            csv_writer.writerow([x,y,w,h])
+            # write face position to a CSV file with the same filename but with .csv extension
+            with open(os.path.join(target_folder, f"{filename}.csv"), "w", newline="") as csv_file:
+                csv_writer = csv.writer(csv_file, delimiter=",")
+                for x,y,w,h in faces:
+                    csv_writer.writerow([x,y,w,h])
 
-                    #change save_frames status
-                    save_frames = False
+            #change save_frames status and reset counter
+            save_frames = False
+            frames_since_detection = 0
 
-                    frames_since_detection = 0
-            #dont save face for 30 frames after face was saved
-            else:
-                frames_since_detection += 1
+        #dont save face for 30 frames after face was saved
+        else:
+            frames_since_detection += 1
 
-                if frames_since_detection >= 30:
-                    save_frames = True
+            if frames_since_detection >= 30:
+                save_frames = True
         
         #display frame
         cv.imshow('frame', frame_with_rectangle)
-    
-            #dieses google drive ding
 
             
         if cv.waitKey(1) == ord('q'):
@@ -117,5 +116,3 @@ def record(args):
     if args.folder is None:
         print("Please specify folder for data to be recorded into")
         exit()
-
-record("kdkg")
